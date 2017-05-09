0dd0536ab24f1b225894e3a72121a08045089cff
李研亭 <liyanting@eduroam-0-184.enst.fr>
2017-05-09 15:37:09 +0200

    Question 12.

--- Occurrence/src/Count.java
+++ Occurrence/src/Count.java
@@ -36,11 +36,11 @@ public class Count {
 //		FilterBizzares50(list,"Question 11");
 		
 //		Question 12:
-		long startTime = System.currentTimeMillis();
-		list = Occurrence_Clean_2("sante_publique_mini","Question 12");
-		long endTime   = System.currentTimeMillis();
-		long totalTime = endTime - startTime;
-		System.out.println("TotalTime: "+totalTime);
+//		list = Occurrence_Clean_2("sante_publique","Question 12");
+		list = Occurrence_test("sante_publique_mini","Q_test");
+		
+//		Question 13:
+		
 
 	}
 	
@@ -52,7 +52,8 @@ public class Count {
 				
 		while((line = bufferedReader.readLine()) != null) {
 			str = str + line + " ";
-			str.replaceAll("\\pP\\pS\\d+", " ");
+//			str.replaceAll("\\pP\\pS\\d+", " ");
+			str = str.replaceAll("[^a-zA-Z0-9éêèëÉÊÈËôÔùÙàÀîÎïÏçÇ\\-]"," ");
 		}
 		
 		String[] words = str.split(" ");
@@ -70,8 +71,8 @@ public class Count {
 		fos = new FileOutputStream(fout);
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
 		
-		System.out.println("--------"+ outputFile +": TreeMap key ascending--------");
-		bw.write("--------"+ outputFile +": TreeMap key ascending--------");
+		System.out.println("----------------"+ outputFile +": TreeMap key ascending----------------");
+		bw.write("----------------"+ outputFile +": TreeMap key ascending----------------");
 		bw.newLine();
 		for (Entry<String, Integer> entry : occurrence.entrySet()) {
 			System.out.println(entry.getKey() + " " + entry.getValue());
@@ -100,13 +101,13 @@ public class Count {
 //		Sort
 		Collections.sort(listOccu, valueComparator);
 //		It output not only words but also ":"
-		System.out.println("--------"+ outputFile +":--------");
-		bw.write("--------"+ outputFile +":--------");
-		bw.newLine();
+		System.out.println("----------------"+ outputFile +":----------------");
+		bw.write("----------------"+ outputFile +":----------------");
+//		bw.newLine();
 		for (Map.Entry<String, Integer> entry: listOccu) {
 			System.out.println(entry.getKey() + " " + entry.getValue());
 			bw.write(entry.getKey() + " " + entry.getValue());
-			bw.newLine();
+//			bw.newLine();
 		}
 		
 		bw.close();
@@ -142,15 +143,17 @@ public class Count {
 		BufferedReader bufferedReader = new BufferedReader(fileReader);
 				
 		while((line = bufferedReader.readLine()) != null) {
-//			System.out.println(line);
-			str = str + line + " ";
-			str = str.replaceAll("[^a-zA-Z0-9éêèëÉÊÈËôÔùÙàÀîÎïÏçÇ\\-]"," ");
+			if(!line.isEmpty()) {
+				str = str + line + " ";
+				str = str.replaceAll("[^a-zA-Z0-9éêèëÉÊÈËôÔùÙàÀîÎïÏçÇ\\_]"," ");	
+			}
 		}
-		str = str.toLowerCase();
+//		str = str.toLowerCase();
 		
 		String[] words = str.split(" ");
 		Map<String, Integer> occurrence = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
 		for(String w : words) {
+			if(w.isEmpty()) continue;
 			Integer n = occurrence.get(w);
 			if(n==null) n=1;
 			else n++;
@@ -165,8 +168,8 @@ public class Count {
 		File fout = new File(outputFile+".txt");
 		FileOutputStream fos = new FileOutputStream(fout);
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
-		System.out.println("--------"+ outputFile +": les 50 mots les plus utilisés--------");
-		bw.write("--------"+ outputFile +": les 50 mots les plus utilisés--------");
+		System.out.println("----------------"+ outputFile +": les 50 mots les plus utilisés----------------");
+		bw.write("----------------"+ outputFile +": les 50 mots les plus utilisés----------------");
 		bw.newLine();
 		int i=50;
 		for (Map.Entry<String, Integer> entry: list) {
@@ -188,8 +191,8 @@ public class Count {
 				"lui","leur","eux","qui","que","quoi","dont","où","mais","ou","et","donc","or","ni","car"};
 	    Set<String> mySet = new HashSet<String>(Arrays.asList(ProCons));
 	    
-		System.out.println("--------"+ outputFile +": filtre les pronoms et conjonctions--------");
-		bw.write("--------"+ outputFile +": filtre les pronoms et conjonctions--------");
+		System.out.println("----------------"+ outputFile +": filtre les pronoms et conjonctions----------------");
+		bw.write("----------------"+ outputFile +": filtre les pronoms et conjonctions----------------");
 		bw.newLine();
 		for (Map.Entry<String, Integer> entry: list) {
 			if(!mySet.contains(entry.getKey())) {
@@ -215,8 +218,8 @@ public class Count {
 		System.arraycopy(Bizzares, 0, WORDS, ProCons.length, Bizzares.length);
 		Set<String> mySet = new HashSet<String>(Arrays.asList(WORDS));
 		
-		System.out.println("--------"+ outputFile +": filtre les mots bizarres--------");
-		bw.write("--------"+ outputFile +": filtre les mots bizarres--------");
+		System.out.println("----------------"+ outputFile +": filtre les mots bizarres----------------");
+		bw.write("----------------"+ outputFile +": filtre les mots bizarres----------------");
 		bw.newLine();
 		int i=50;
 		for (Map.Entry<String, Integer> entry: list) {
@@ -235,26 +238,33 @@ public class Count {
 		File fout = new File(outputFile+".txt");
 		FileOutputStream fos = new FileOutputStream(fout);
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
-		System.out.println("--------"+ outputFile +"--------");
-		bw.write("--------"+ outputFile +"--------");
+		System.out.println("----------------"+ outputFile +"----------------");
+		bw.write("----------------"+ outputFile +"----------------");
 		
+		long startTime = System.currentTimeMillis();
 		String str = new String();
 		String line = null;
 		FileReader fileReader = new FileReader(fileName+".txt");
 		BufferedReader bufferedReader = new BufferedReader(fileReader);
 		
-		long startTime = System.currentTimeMillis();
+		long startTimeRead = System.currentTimeMillis();
 		while((line = bufferedReader.readLine()) != null) {
-			line = line.replaceAll("[^a-zA-Z0-9éêèëÉÊÈËôÔùÙàÀîÎïÏçÇ\\-]"," ");
-			str = str + line + " ";
+			if(!line.isEmpty()) {
+				line = line.replaceAll("[^a-zA-Z0-9éêèëÉÊÈËôÔùÙàÀîÎïÏçÇ\\-\\_]"," ");
+				str += line + " ";
+			}
 		}
+		bufferedReader.close();
 		str = str.toLowerCase();
+		long endTimeRead  = System.currentTimeMillis();
+		long totalTimeRead = endTimeRead - startTimeRead; 
 		
+		long startTimeSplit = System.currentTimeMillis();
 		String[] ProCons =  {"le","la","les","on","l","je","tu","il","elle","nous","vous","ils","elles",
 				"lui","leur","eux","qui","que","quoi","dont","où","mais","ou","et","donc","or","ni",
 				"car","ce","qu","d","s","tout","tous","si","cette"};
 		String[] Bizzares = {"II","de","des","à","du","en","un","une","dans","est","par","au","sont",
-				"sur","ne","a","être","ses","son","pas","ii","ier","n"};
+				"sur","ne","a","être","ses","son","pas","ii","ier","n","-","_"};
 		Set<String> mySet_ProCons = new HashSet<String>(Arrays.asList(ProCons));
 		Set<String> mySet_Bizzares = new HashSet<String>(Arrays.asList(Bizzares));
 		Set<String> mySet = new HashSet<String>();
@@ -262,22 +272,24 @@ public class Count {
 		mySet.addAll(mySet_Bizzares);
 
 		String[] words = str.split(" ");
-		Map<String, Integer> occurrence = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+		long endTimeSplit  = System.currentTimeMillis();
+		long totalTimeSplit = endTimeSplit - startTimeSplit; 
 		
+		long startTimeAdd = System.currentTimeMillis();
+		Map<String, Integer> occurrence = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
 		for(String w : words) {
 			if(mySet.contains(w)) continue;
 			if(w.matches(".*\\d+.*")) continue;
-			if(w==null || w=="") continue;
+			if(w.isEmpty()) continue;
 			Integer n = occurrence.get(w);
 			if(n==null) n=1;
 			else n++;
 			occurrence.put(w, n);
 		}
-		long endTime   = System.currentTimeMillis();
-		long totalTime = endTime - startTime;
-		System.out.println("Time Nettoyer/filtrer and Compter: " + totalTime);
+		long endTimeAdd  = System.currentTimeMillis();
+		long totalTimeAdd = endTimeAdd - startTimeAdd;
 		
-		startTime = System.currentTimeMillis();
+		long startTimeTri = System.currentTimeMillis();
 //		Descending Comparator
 		Comparator<Map.Entry<String, Integer>> valueComparator = new Comparator<Map.Entry<String, Integer>>() {
 			@Override
@@ -289,23 +301,219 @@ public class Count {
 		List<Map.Entry<String,Integer>> list = new ArrayList<Map.Entry<String, Integer>>(occurrence.entrySet());
 //		Sort
 		Collections.sort(list, valueComparator);
-		endTime   = System.currentTimeMillis();
-		totalTime = endTime - startTime;
-		System.out.println("Time Tri: " + totalTime);
-		bufferedReader.close();
+		long endTimeTri   = System.currentTimeMillis();
+		long totalTimeTri = endTimeTri - startTimeTri;
 		
-		bw.newLine();
+//		bw.newLine();
 		int i=50;
 		for (Map.Entry<String, Integer> entry: list) {
 			System.out.println(entry.getKey() + " " + entry.getValue());
 			bw.write(entry.getKey() + " " + entry.getValue());
 			i--;
-			bw.newLine();
+//			bw.newLine();
 			if(i<0) break;
 		}
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+
+		System.out.println("\n----------Time Use----------");
+		System.out.println("Time Lire et Nettoyer: " + totalTimeRead/1000. + " s");
+		System.out.println("Time Split: " + totalTimeSplit/1000. + " s");
+		System.out.println("Time filtrer and Compter: " + totalTimeAdd/1000. + " s");
+		System.out.println("Time Tri: " + totalTimeTri/1000. + " s");
+		System.out.println("TotalTime: "+totalTime/1000. + " s");
+
+		bw.write("\n----------Time Use----------");
+		bw.write("Time Lire et Nettoyer: " + totalTimeRead/1000. + " s");
+		bw.write("Time Split: " + totalTimeSplit/1000. + " s");
+		bw.write("Time filtrer and Compter: " + totalTimeAdd/1000. + " s");
+		bw.write("Time Tri: " + totalTimeTri/1000. + " s");
+		bw.write("TotalTime: "+totalTime/1000. + " s");
+
 		bw.close();
 		return list;
 	}
+	
+	public static List<Map.Entry<String,Integer>> Occurrence_Clean_SansNyt(String fileName, String outputFile) throws IOException {
+		File fout = new File(outputFile+".txt");
+		FileOutputStream fos = new FileOutputStream(fout);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+		System.out.println("----------------"+ outputFile +"----------------");
+		bw.write("----------------"+ outputFile +"----------------");
+		
+		long startTime = System.currentTimeMillis();
+		String str = new String();
+		String line = null;
+		FileReader fileReader = new FileReader(fileName+".txt");
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		
+		long startTimeRead = System.currentTimeMillis();
+		while((line = bufferedReader.readLine()) != null) {
+			if(!line.isEmpty()) {
+//				line = line.replaceAll("[^a-zA-Z0-9\\-\\_]"," ");
+				str += line + " ";
+			}
+		}
+		bufferedReader.close();
+		str = str.toLowerCase();
+		String[] words = str.split(" ");
+		long endTimeRead  = System.currentTimeMillis();
+		long totalTimeRead = endTimeRead - startTimeRead;
+		
+		long startTimeAdd = System.currentTimeMillis();
+		Map<String, Integer> occurrence = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+		for(String w : words) {
+//			if(w.matches(".*\\d+.*")) continue;
+			if(w.isEmpty()) continue;
+			Integer n = occurrence.get(w);
+			if(n==null) n=1;
+			else n++;
+			occurrence.put(w, n);
+		}
+		long endTimeAdd  = System.currentTimeMillis();
+		long totalTimeAdd = endTimeAdd - startTimeAdd;
+		
+		long startTimeTri = System.currentTimeMillis();
+//		Descending Comparator
+		Comparator<Map.Entry<String, Integer>> valueComparator = new Comparator<Map.Entry<String, Integer>>() {
+			@Override
+			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+				return o2.getValue() - o1.getValue();
+			}
+		};
+//		Map to List
+		List<Map.Entry<String,Integer>> list = new ArrayList<Map.Entry<String, Integer>>(occurrence.entrySet());
+//		Sort
+		Collections.sort(list, valueComparator);
+		long endTimeTri   = System.currentTimeMillis();
+		long totalTimeTri = endTimeTri - startTimeTri;
+		
+		int i=50;
+		for (Map.Entry<String, Integer> entry: list) {
+			System.out.println(entry.getKey() + " " + entry.getValue());
+			bw.write(entry.getKey() + " " + entry.getValue());
+			i--;
+			if(i<0) break;
+		}
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+
+		System.out.println("\n----------Time Use----------");
+		System.out.println("Time Nettoyer: " + totalTimeRead/1000. + " s");
+		System.out.println("Time filtrer and Compter: " + totalTimeAdd/1000. + " s");
+		System.out.println("Time Tri: " + totalTimeTri/1000. + " s");
+		System.out.println("TotalTime: "+totalTime/1000. + " s");
+
+		bw.write("\n----------Time Use----------");
+		bw.write("Time Nettoyer: " + totalTimeRead/1000. + " s");
+		bw.write("Time filtrer and Compter: " + totalTimeAdd/1000. + " s");
+		bw.write("Time Tri: " + totalTimeTri/1000. + " s");
+		bw.write("TotalTime: "+totalTime/1000. + " s");
+
+		bw.close();
+		return list;
+	}
+
+	public static List<Map.Entry<String,Integer>> Occurrence_test(String fileName, String outputFile) throws IOException {
+		File fout = new File(outputFile+".txt");
+		FileOutputStream fos = new FileOutputStream(fout);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+		System.out.println("----------------"+ outputFile +"----------------");
+		bw.write("----------------"+ outputFile +"----------------");
+		
+		long startTime = System.currentTimeMillis();
+		String str = new String();
+		String line = null;
+		FileReader fileReader = new FileReader(fileName+".txt");
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		
+		long startTimeRead = System.currentTimeMillis();
+		while((line = bufferedReader.readLine()) != null) {
+			if(!line.isEmpty()){
+				line = line.replaceAll("[^a-zA-Z0-9éêèëÉÊÈËôÔùÙàÀîÎïÏçÇ\\-\\_]"," ");
+				str += line + " ";
+			}
+		}
+		bufferedReader.close();
+		str = str.toLowerCase();
+		long endTimeRead  = System.currentTimeMillis();
+		long totalTimeRead = endTimeRead - startTimeRead; 
+		
+		long startTimeSplit = System.currentTimeMillis();
+		String[] ProCons =  {"le","la","les","on","l","je","tu","il","elle","nous","vous","ils","elles",
+				"lui","leur","eux","qui","que","quoi","dont","où","mais","ou","et","donc","or","ni",
+				"car","ce","qu","d","s","tout","tous","si","cette"};
+		String[] Bizzares = {"II","de","des","à","du","en","un","une","dans","est","par","au","sont",
+				"sur","ne","a","être","ses","son","pas","ii","ier","n","-","_"};
+		Set<String> mySet_ProCons = new HashSet<String>(Arrays.asList(ProCons));
+		Set<String> mySet_Bizzares = new HashSet<String>(Arrays.asList(Bizzares));
+		Set<String> mySet = new HashSet<String>();
+		mySet.addAll(mySet_ProCons);
+		mySet.addAll(mySet_Bizzares);
+
+		String[] words = str.split(" ");
+		long endTimeSplit  = System.currentTimeMillis();
+		long totalTimeSplit = endTimeSplit - startTimeSplit; 
+		
+		long startTimeAdd = System.currentTimeMillis();
+		Map<String, Integer> occurrence = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+		for(String w : words) {
+			if(mySet.contains(w)) continue;
+			if(w.matches(".*\\d+.*")) continue;
+			if(w.isEmpty()) continue;
+			Integer n = occurrence.get(w);
+			if(n==null) n=1;
+			else n++;
+			occurrence.put(w, n);
+		}
+		long endTimeAdd  = System.currentTimeMillis();
+		long totalTimeAdd = endTimeAdd - startTimeAdd;
+		
+		long startTimeTri = System.currentTimeMillis();
+//		Descending Comparator
+		Comparator<Map.Entry<String, Integer>> valueComparator = new Comparator<Map.Entry<String, Integer>>() {
+			@Override
+			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+				return o2.getValue() - o1.getValue();
+			}
+		};
+//		Map to List
+		List<Map.Entry<String,Integer>> list = new ArrayList<Map.Entry<String, Integer>>(occurrence.entrySet());
+//		Sort
+		Collections.sort(list, valueComparator);
+		long endTimeTri   = System.currentTimeMillis();
+		long totalTimeTri = endTimeTri - startTimeTri;
+		
+//		bw.newLine();
+		int i=50;
+		for (Map.Entry<String, Integer> entry: list) {
+			System.out.println(entry.getKey() + " " + entry.getValue());
+			bw.write(entry.getKey() + " " + entry.getValue());
+			i--;
+//			bw.newLine();
+			if(i<0) break;
+		}
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+
+		System.out.println("\n----------Time Use----------");
+		System.out.println("Time Lire et Nettoyer: " + totalTimeRead/1000. + " s");
+		System.out.println("Time Split: " + totalTimeSplit/1000. + " s");
+		System.out.println("Time filtrer and Compter: " + totalTimeAdd/1000. + " s");
+		System.out.println("Time Tri: " + totalTimeTri/1000. + " s");
+		System.out.println("TotalTime: "+totalTime/1000. + " s");
+
+		bw.write("\n----------Time Use----------");
+		bw.write("Time Lire et Nettoyer: " + totalTimeRead/1000. + " s");
+		bw.write("Time Split: " + totalTimeSplit/1000. + " s");
+		bw.write("Time filtrer and Compter: " + totalTimeAdd/1000. + " s");
+		bw.write("Time Tri: " + totalTimeTri/1000. + " s");
+		bw.write("TotalTime: "+totalTime/1000. + " s");
+
+		bw.close();
+		return list;
+	}
+
 }
 
 
